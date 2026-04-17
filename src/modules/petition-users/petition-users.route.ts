324ebc32c6dc0d4e2f70234007b9a1c4ac154ce4
luk <example@example.com>
2026-04-17 19:56:05 +0200

    user delete feature

--- src/modules/petition-users/petition-users.route.ts
+++ src/modules/petition-users/petition-users.route.ts
@@ -1,11 +1,13 @@
 import express, { type Router } from "express";
-import { login, register } from "./petition-users.controller.js";
+import { login, register, deleteUser } from "./petition-users.controller.js";
 import { validateData } from "../../shared/middleware/validation.middleware.js";
 import { loginSchema, registerSchema } from "./petition-users.schema.js";
+import { verifyToken } from "../../shared/middleware/jwt.middleware.js";
 
 const router: Router = express.Router();
 
 router.post("/login", validateData(loginSchema), login);
 router.post("/register", validateData(registerSchema), register);
+router.delete("/delete", verifyToken ,deleteUser);
 
 export default router;
