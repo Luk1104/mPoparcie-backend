fd62b6965a87963df156ea6488ea6d0093857105
Krol-Kacper <279415@student.pwr.edu.pl>
2026-05-11 00:28:10 +0200

    add endpoint for voting

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import petitionUsersRoute from "./modules/petition-users/petition-users.route.js";
 import petitionCrudRoute from "./modules/petition-crud/petition-crud.route.js";
 import zkpRoute from "./modules/zkp-users/zkp-users.route.js";
+import zkpVote from "./modules/voting/voting.route.js";
 import { errorHandler } from "./shared/middleware/error.middleware.js";
 
 const app = express();
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use("/api/petition/user", petitionUsersRoute);
 app.use("/api/petition", petitionCrudRoute);
 app.use("/api/zkp", zkpRoute);
+app.use("/api/vote", zkpVote);
 
 app.use(errorHandler);
 
