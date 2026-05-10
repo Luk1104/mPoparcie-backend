2af2a95858a57b5ab7592bcd798db3654769bbf9
Krol-Kacper <279415@student.pwr.edu.pl>
2026-05-11 00:27:41 +0200

    currentroot is not saved anymore, deprecated fix

--- src/modules/zkp-users/merkle-tree-functions.ts
+++ src/modules/zkp-users/merkle-tree-functions.ts
@@ -1,9 +1,10 @@
 import { Group } from "@semaphore-protocol/group";
-import { ZkpCommitmentModel, ZkpMetadataModel} from "./merkle-tree.model.js";
+import { ZkpCommitmentModel, ZkpMetadataModel } from "./merkle-tree.model.js";
 
 export const addMemberToTree = async (
-    commitment: string,
-     groupId: string = "1") => {
+  commitment: string,
+  groupId: string = "1",
+) => {
   try {
     // 1. Znajdź użytkownika z najwyższym indeksem w danej grupie
     const lastMember = await ZkpCommitmentModel.findOne({ groupId })
@@ -13,25 +14,31 @@ export const addMemberToTree = async (
     // 2. Ustal nowy indeks (jeśli to pierwszy użytkownik, daj mu 0)
     const nextIndex = lastMember ? lastMember.index + 1 : 0;
 
+    // const group = await buildSemaphoreGroup();
+    // const currentRoot = group.root;
     // 3. Zapisz w bazie
     const newZkpCommitment = await ZkpCommitmentModel.create({
       commitment,
       index: nextIndex,
-      groupId
+      groupId,
+      //currentRoot,
     });
 
     console.log(`Dodano do drzewa na pozycji: ${nextIndex}`);
     //return newZkpCommitment;
-
   } catch (error) {
     console.error("Błąd zapisu do drzewa:", error);
     throw new Error("Nie udało się dodać użytkownika do Drzewa Merkle'a");
   }
 };
 
-export const buildSemaphoreGroup = async (groupId: string = "1"): Promise<Group> => {
+export const buildSemaphoreGroup = async (
+  groupId: string = "1",
+): Promise<Group> => {
   //Pobierz WSZYSTKICH członków z bazy, POSORTOWANYCH po indeksie rosnąco
-  const members = await ZkpCommitmentModel.find({ groupId }).sort("index").exec();
+  const members = await ZkpCommitmentModel.find({ groupId })
+    .sort("index")
+    .exec();
 
   // Może id groupy trzeba podać i głębokość
   const group = new Group();
@@ -43,11 +50,11 @@ export const buildSemaphoreGroup = async (groupId: string = "1"): Promise<Group>
   }
 
   // Aktualizuj metadane (aktualny korzeń)
-    const metadata = await ZkpMetadataModel.findOneAndUpdate(
-      { groupId },
-      { currentRoot: group.root },
-      { upsert: true, new: true }
-    );
+  const metadata = await ZkpMetadataModel.findOneAndUpdate(
+    { groupId },
+    { currentRoot: group.root },
+    { upsert: true, returnDocument: "after" },
+  );
   //group to teraz całe drzewo
 
   //console.log("Aktualny korzeń:", group.root); juz jest wypisany w service
