634cc6fa538a1e0318ebd8d790169999ad4da40d
Krol-Kacper <279415@student.pwr.edu.pl>
2026-05-22 21:22:07 +0200

    identt blind register (in-testing)

--- src/modules/zkp-users/zkp-users.controller.ts
+++ src/modules/zkp-users/zkp-users.controller.ts
@@ -5,15 +5,19 @@ import {
 } from "./zkp-users.service.js";
 import type { RegisterDTO } from "./zkp-users.schema.js";
 import { zkpTreeDumpService } from "./zkp-users.service.js";
+import { webhookEmitter } from "./zkp-users.service.js";
+import crypto from "crypto";
+import { generateLinkService } from "./zkp-users.service.js";
 
 export const zkprequestuserHash = async (
-  req: Request,
+  req: Request<{ document_id: string }, any, any>,
   res: Response,
   next: NextFunction,
 ) => {
   try {
     // w tokenie jest userHash
-    const token = await zkprequestuserHashService();
+    const document_id = req.params.document_id;
+    const token = await zkprequestuserHashService(document_id);
     if (token) {
       res.cookie("token", token, {
         httpOnly: true,
@@ -26,7 +30,7 @@ export const zkprequestuserHash = async (
 
     return res
       .status(201)
-      .json({ status: "success", message: "Rejestracja/1 udana" });
+      .json({ status: "success", message: "Rejestracja/2 udana" });
   } catch (error) {
     next(error);
   }
@@ -48,7 +52,43 @@ export const zkpregister = async (
     const state = await zkpregisterService(userHash, commitment);
     return res
       .status(201)
-      .json({ status: "success", message: "Rejestracja/2 udana" });
+      .json({ status: "success", message: "Rejestracja/3 udana" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const generateLink = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const linkData = await generateLinkService();
+    return res.status(200).json({
+      status: "success",
+      message: "Rejestracja/1 udana",
+      data: linkData,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const webhookReceiver = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { document_id } = req.body;
+    const userHash = crypto
+      .createHash("sha256")
+      .update(document_id)
+      .digest("hex");
+    console.log(req.body);
+    webhookEmitter.emit(document_id, { userHash }); //should be changed to real userhash later on
+    return res.status(200).send("Webhook received");
   } catch (error) {
     next(error);
   }
